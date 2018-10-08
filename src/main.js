0fc027c6a98ee0cd6e5b2a5318780b4d5969f60c
Vinícius Marcili <vinipetter@gmail.com>
2018-10-08 08:24:39 -0300

    Add Boostrap and FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
+
+/* Bootstrap */
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+/* Bootstrap */
+
+/* Font Awesome */
+library.add(faCoffee)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+/* Font Awesome */
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
